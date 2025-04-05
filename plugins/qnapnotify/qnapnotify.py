460c615c483cbf838055611b8e11653f19a28619
lwgm <40270465+lwgm@users.noreply.github.com>
2025-04-05 21:21:38 +0800

    2

--- plugins/qnapnotify/qnapnotify.py
+++ plugins/qnapnotify/qnapnotify.py
@@ -118,7 +118,7 @@ class QnapNotify(_PluginBase):
     # 插件图标
     plugin_icon = "https://github.com/jxxghp/MoviePilot-Plugins/tree/main/icons/Qnap_A.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "lwgm"
     # 作者主页
